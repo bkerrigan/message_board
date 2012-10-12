d6df4a381eccf90650f49298f8c4c55844309993
Bryan Kerrigan <bryankerrigan@gmail.com>
2012-10-12 14:12:58 -0700

    Updating some designs and functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 MessageBoard::Application.routes.draw do
   resources :posts
 
-  get "home/index"
+  get "posts/index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -52,7 +52,7 @@ MessageBoard::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'posts#index'
 
   # See how all your routes lay out with "rake routes"
 
